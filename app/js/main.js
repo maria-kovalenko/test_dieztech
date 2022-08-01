faa3ab27cf542c41a535586199fc0ff487f5c696
Maria <m1a2r3.kovalenko@gmail.com>
2022-08-01 22:43:53 +0300

    ..

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,7 @@ const swiperProfession = new Swiper(".profession-slider", {
     el: ".swiper-pagination",
 
     clickable: true,
+    // dynamicBullets: true,
   },
 
   navigation: {
@@ -16,25 +17,54 @@ const swiperProfession = new Swiper(".profession-slider", {
   spaceBetween: 30,
 });
 
-const burgerMenu = document.querySelector(".page-header__toggle");
-const burgerMenuOpened = document.querySelector(".page-header__toggle--opened");
+// -------------slider events------------------
+
+// const sliderEvents = document.querySelector(".slider-container");
+
+// let mySwiper;
+
+// function mobileSlider() {
+//   if (window.innerWidth <= 673 && sliderEvents.dataset.mobile === "false") {
+//     mySwiper = new Swiper(sliderEvents, {
+//       direction: "horizontal",
+//       slidesPerView: 1,
+//       spaceBetween: 30,
+
+//       slideClass: "events__slide",
+//     });
+
+//     sliderEvents.dataset.mobile === "true";
+//   }
+
+//   if (window.innerWidth > 673) {
+//     sliderEvents.dataset.mobile === "false";
+
+//     if (sliderEvents.classList.contains("swiper-container-initialized")) {
+//       mySwiper.destroy();
+//     }
+//   }
+// }
+
+// mobileSlider();
+// window.addEventListener("resize", () => {
+//   mobileSlider();
+// });
+
+const buttonOpen = document.querySelector(".page-header__toggle--opened");
+const buttonClose = document.querySelector(".page-header__toggle--closed");
 const mobileMenu = document.querySelector(".page-header__navigation");
 
 let condition = "close";
 
-burgerMenu.addEventListener("click", function () {
+buttonClose.addEventListener("click", function () {
   if (condition === "close") {
+    buttonClose.classList.remove("page-header__toggle--closed");
+    buttonClose.classList.add("page-header__toggle--opened");
     mobileMenu.style.display = "block";
-    burgerMenu.classList.remove("page-header__toggle--closed");
-    burgerMenu.classList.add("page-header__toggle--opened");
     condition = "open";
-  }
-});
-
-burgerMenuOpened.addEventListener("click", function () {
-  if (condition === "open") {
-    burgerMenu.classList.remove("page-header__toggle--opened");
-    burgerMenu.classList.add("page-header__toggle--closed");
+  } else {
+    buttonClose.classList.remove("page-header__toggle--opened");
+    buttonClose.classList.add("page-header__toggle--closed");
     mobileMenu.style.display = "none";
     condition = "close";
   }
