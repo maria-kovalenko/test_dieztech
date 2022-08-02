cde575598e221a87674778aff3f0ad55020ae982
Maria <m1a2r3.kovalenko@gmail.com>
2022-08-02 12:09:19 +0300

    готовый проект

--- js/main.js
+++ js/main.js
@@ -25,39 +25,6 @@ const swiperProfession = new Swiper(".profession-slider", {
   },
 });
 
-// -------------slider events------------------
-
-// const sliderEvents = document.querySelector(".slider-container");
-
-// let mySwiper;
-
-// function mobileSlider() {
-//   if (window.innerWidth <= 673 && sliderEvents.dataset.mobile === "false") {
-//     mySwiper = new Swiper(sliderEvents, {
-//       direction: "horizontal",
-//       slidesPerView: 1,
-//       spaceBetween: 30,
-
-//       slideClass: "events__slide",
-//     });
-
-//     sliderEvents.dataset.mobile === "true";
-//   }
-
-//   if (window.innerWidth > 673) {
-//     sliderEvents.dataset.mobile === "false";
-
-//     if (sliderEvents.classList.contains("swiper-container-initialized")) {
-//       mySwiper.destroy();
-//     }
-//   }
-// }
-
-// mobileSlider();
-// window.addEventListener("resize", () => {
-//   mobileSlider();
-// });
-
 const buttonOpen = document.querySelector(".page-header__toggle--opened");
 const buttonClose = document.querySelector(".page-header__toggle--closed");
 const mobileMenu = document.querySelector(".page-header__navigation");
